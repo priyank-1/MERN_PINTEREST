a62ac881d86e1df6f2fad01a8deff44401acb49e
PRIYANK 1 <singhumang444@gmail.com>
2023-11-10 14:17:49 +0530

    Added homepage response

--- server/index.js
+++ server/index.js
@@ -20,6 +20,9 @@ app.use(cors());
 app.use('/posts',postRoutes);
 app.use('/users', userRoutes);
 
+app.get('/',(req,res)=>{
+    res.send('APP IS RUNNING');
+})
 
 const mongoURI = process.env.MONGODB_URI;
 const PORT = process.env.PORT ;
